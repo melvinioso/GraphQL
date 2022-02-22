fd3384843dd4e29484e6d2001359346facdc2221
Matt McGavern <matt.mcgavern@gmail.com>
2022-02-22 11:41:55 -0600

    SongList component

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -1,20 +1,31 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class SongList extends Component {
-  render () {
-    console.log(this.props);
+  renderSongs() {
+    return this.props.data.songs.map((song) => {
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      );
+    });
+  }
 
-    return (
-      <div>SongList</div>
-    )
+  render() {
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
+    }
+
+    return <ul className="collection">{this.renderSongs()}</ul>;
   }
 }
 
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
