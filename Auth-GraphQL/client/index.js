f2872eb3e6957c4a0b1edeb5ca3c098fc9febfbc
Matt McGavern <matt.mcgavern@gmail.com>
2022-02-24 12:35:26 -0600

    react router root routes

--- Auth-GraphQL/client/index.js
+++ Auth-GraphQL/client/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+
+import App from './components/App';
 
 // Uniquely identify every record we fetch
 const client = new ApolloClient({
@@ -11,7 +14,9 @@ const client = new ApolloClient({
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Auth Starter</div>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}></Route>
+      </Router>
     </ApolloProvider>
   );
 };
