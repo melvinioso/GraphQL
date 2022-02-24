7f2bbf6c0ccb47574ca6e4f5306dc4d58c4ee1e1
Matt McGavern <matt.mcgavern@gmail.com>
2022-02-24 13:34:01 -0600

    authentication state

--- Auth-GraphQL/client/components/Header.js
+++ Auth-GraphQL/client/components/Header.js
@@ -3,13 +3,33 @@ import {graphql} from 'react-apollo';
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
-  render() {
-    console.log(this.props.data);
+  renderButtons() {
+    const { loading, user } = this.props.data;
 
-    return (
+    if (loading) { return <div />; }
+
+    if (user) {
+      return (
       <div>
-        Header
+        Logout
       </div>
+      )
+    } else {
+      return (
+        <div>
+          You're not signed in.
+        </div>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <nav >
+        <div className="nav-wrapper">
+          {this.renderButtons()}
+        </div>
+      </nav>
     );
   }
 }
